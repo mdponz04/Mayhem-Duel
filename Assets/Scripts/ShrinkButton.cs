38abfcaf8f7318c56150920c7cbdea5d1c734a1f
underwork <lenamquoc1@gmail.com>
2024-10-01 11:53:25 +0700

    Add base script

---
+++ Assets/Scripts/ShrinkButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkButton : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hello There");
+    }
+}
