5dae8f62389d92c2ad65659b80db5063fd942157
underwork <lenamquoc1@gmail.com>
2024-10-11 11:10:57 +0700

    Added UI Elements
    - Added A functional UI Menu to control Day Simulator's speed
    - Added an In-game instruction

--- Assets/Scripts/NQ/DaySimulator.cs
+++ Assets/Scripts/NQ/DaySimulator.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class DaySimulator : MonoBehaviour
 {
     // How fast day and night passed
-    public float rotateSpeed = 0.1f;
+    private float rotateSpeed = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
         // Rotate on X axis only
         transform.Rotate(rotateSpeed, 0, 0);
+    }
+
+    public void ChangeSpeed(float speed)
+    {
+        rotateSpeed = speed;
     }
 }
