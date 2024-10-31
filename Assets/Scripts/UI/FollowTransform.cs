b54ef68270f43679d01aba7d0c04160649260141
underwork <lenamquoc1@gmail.com>
2024-10-31 20:03:36 +0700

    Added more UI features
    - Make menus follow in front of player (Beta version)
    -Added Game Over menu
    -Added UI canvas to TD scene
    -Rename some prefabs

---
+++ Assets/Scripts/UI/FollowTransform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    [SerializeField] private Transform lookAt;
+    [SerializeField] private Transform transformToFollow;
+
+    public float followSpeed = 3f;
+
+    private Transform _thisTransfrom;
+    private void Start()
+    {
+        // Cache this lookup
+        _thisTransfrom = transform;
+    }
+
+    private void Update()
+    {
+        //_thisTransfrom.LookAt(lookAt, Vector3.up);
+
+        var newPosition = _thisTransfrom.position;
+        var followPosition = transformToFollow.position;
+        newPosition.x = Mathf.Lerp(newPosition.x, followPosition.x, followSpeed * Time.deltaTime);
+        //newPosition.y = Mathf.Lerp(newPosition.y, followPosition.y, followSpeed * Time.deltaTime);
+        newPosition.z = Mathf.Lerp(newPosition.z, (followPosition.z - 0.5f), followSpeed * Time.deltaTime);
+
+        transform.position = newPosition;
+    }
+
+}
