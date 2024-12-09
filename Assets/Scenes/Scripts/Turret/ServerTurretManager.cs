88191c6a263f994aae02f31b9b6f62d6fb580697
AugustanHalo <tannd1008@gmail.com>
2024-12-09 13:45:54 +0700

    Sync the repo

---
+++ Assets/Scenes/Scripts/Turret/ServerTurretManager.cs
@@ -0,0 +1,28 @@
+using Assets.Scripts.TheCastle;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerTurretManager : MonoBehaviour
+{
+    public List<TurretType> turretTypes;
+    public List<PlacedObjectTypeSO> turretPfs;
+    private Dictionary<TurretType, PlacedObjectTypeSO> turretMap;
+
+    private void Awake()
+    {
+        turretMap = new();
+
+        for (int i = 0; i < turretTypes.Count; i++)
+        {
+            if (!turretMap.ContainsKey(turretTypes[i]))
+            {
+                turretMap.Add(turretTypes[i], turretPfs[i]);
+            }
+        }
+    }
+
+    public PlacedObjectTypeSO GetTurretPf(TurretType turretType)
+    {
+        return turretMap[turretType];
+    }
+}
